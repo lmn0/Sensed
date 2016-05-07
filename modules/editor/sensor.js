6f3b2040800e4c9a09559d3c9f2eb31cd271c234
narasakumar <knarasa@gmail.com>
2016-05-06 18:06:50 -0700

    Subscription_Improved

--- modules/editor/sensor.js
+++ modules/editor/sensor.js
@@ -120,9 +120,51 @@ router.post(['/', '/:action'], function(req, res, next) {
 		var found=0;
 		console.log("===="+userdoc._id);
 
-		console.log("=========req.body.check==================");
-		console.log(req.body.check);
-		console.log("===========================");
+		//console.log("=========req.body.check==================");
+		//console.log(req.body.check);
+		//console.log("===========================");
+
+		//console.log("=========req.body.hidden==================");
+		//console.log(req.body.hidden);
+		//console.log("===========================");
+
+		var selectedElements= new Array();;
+		var array = req.body.hidden.split(',');
+
+		for(p=0;p<req.body.check.length;p++)
+		{
+			//console.log("=========array elements==================");
+			var checkIndex=req.body.check[p];
+			//console.log("req.body.check[p] :"+checkIndex);
+			//console.log("array[checkIndex] :"+array[checkIndex]);
+
+			selectedElements[p]=array[checkIndex];
+
+			//console.log("========================================");
+
+		}
+
+
+
+//		for(q=0;q<selectedElements.length;q++)
+//			{
+//			console.log("SELECTED FINAL ELEMETS");
+//			console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+//			console.log("selectedElements["+q+"]: "+selectedElements[q]);
+//
+//			}
+
+		/*for (x=0;x<req.body.check.length;x++)
+			{
+
+			console.log("=========array elements==================");
+			console.log(array[checkIndex[x]]);
+			console.log("===========================");
+
+
+			}*/
+
+
 
 		//console.log(req.body.dateFrom);
 		//console.log(req.body.dateTo);
@@ -139,60 +181,43 @@ router.post(['/', '/:action'], function(req, res, next) {
 		var tempSensorLat="";
 		var tempSensorLng="";
 		var userid="";
-		var tempFrom=req.body.dateFrom;
-		var tempTo=req.body.dateTo;
+		var tempFrom=new Date(req.body.dateFrom);
+		var tempTo=new Date(req.body.dateTo);
 		var te="";
 
-		console.log("Type of check is"+typeof(req.body.check));
+		console.log("Type of selectedElements is"+typeof(selectedElements));
 
 
-		if(typeof req.body.check != typeof "string"){
-			for(i=0;i<req.body.check.length;i++)
+		if(typeof selectedElements != typeof "string"){
+			for(i=0;i<selectedElements.length;i++)
 			{
 				console.log("----------------------------------");
 				console.log("----------------------------------");
 
-				tempSensorDataSetID=req.body.check[i].split("+")[0];
-				tempSensorLat=req.body.check[i].split("+")[1];
-				tempSensorLng=req.body.check[i].split("+")[2];
-				tempSensorId=req.body.check[i].split("+")[3];
+				tempSensorStation=selectedElements[i].split("+")[0];
+				tempSensorDataSetID=selectedElements[i].split("+")[1];
+				tempSensorLat=Number(selectedElements[i].split("+")[2]);
+				tempSensorLng=Number(selectedElements[i].split("+")[3]);
+				tempSensorId=selectedElements[i].split("+")[4];
+				tempSensorName=selectedElements[i].split("+")[5];
+
 
 				userid="571dc7758e70a5e6101dcac1";
-				tempSensorStation="HARD CODED";
-				tempSensorName="HARD CODED";
 
 				console.log("Here comes the BOOOMMM");
 
-				/*var sensorFromDB=db.collection('sensors').aggregate(
-					    { $match : {
-					     "features.sensor": tempSensorId,"dataSetId": tempSensorDataSetID
-					  }},
-					  { $unwind : "$features" },
-					  { $match : {
-					     "features.sensor": tempSensorId
-					  }}
-					);*/
+				console.log("tempSensorStation: "+tempSensorStation);
+				console.log("tempSensorDataSetID: "+tempSensorDataSetID);
+				console.log("tempSensorLat: "+tempSensorLat);
+				console.log("tempSensorLng: "+tempSensorLng);
+				console.log("tempSensorId: "+tempSensorId);
+				console.log("tempSensorName: "+tempSensorName);
+
+				console.log("----------------------------------");
+				console.log("----------------------------------");
 
 
-				//console.log(sensorFromDB);
-
-
-
-
-				/*var cursor =db.collection('sensors').distinct( "features.sensorname", {$and:[ {"dataSetId": tempSensorDataSetID},{"features.sensor":tempSensorId} ]}).toArray(function(err,result){
-					console.log("->" + JSON.stringify(result));
-					if(err==null){
-
-					}
-					else{
-
-					}
-				}
-				);*/
-
-
-
-
+				/*//Hard coded--- Only for testing purpose pls
 					//tempSensorDataSetID="gov_noaa_nws_kmry";
 					//tempSensorLat="36.5833333333333";
 					//tempSensorLng="-121.85";
@@ -202,6 +227,7 @@ router.post(['/', '/:action'], function(req, res, next) {
 
 					//tempSensorName="Air Temperature (Degree F)";
 					//tempSensorId="air_temperature";
+				 	*/
 
 					/*console.log("tempSensorStation: "+tempSensorStation);
 					console.log("tempSensorDataSetID: "+tempSensorDataSetID);
@@ -214,8 +240,6 @@ router.post(['/', '/:action'], function(req, res, next) {
 					console.log("----------------------------------");*/
 
 
-					//var cursor =db.collection('subscription').find("station", { latitude: 38.0657, longitude :-122.2302}).toArray(function(err,result){
-					//var dataSetId =db.collection('subscription').find("dataSetId",{"userid": "571dc7758e70a5e6101dcac1", "dataSetId": tempSensorDataSetID}).toArray(function(err,result){
 					db.collection('subscription').distinct( "dataSetId", {"userid": userid, "dataSetId": tempSensorDataSetID} ,function(err, result) {
 
 
@@ -248,8 +272,7 @@ router.post(['/', '/:action'], function(req, res, next) {
 										if(result1==tempSensorDataSetID)
 										{
 											//Sensor Name found
-											//Do nothing
-
+											//Update the date values--NOT WORKING!!! :(
 											console.log("Updating");
 
 											db.collection('subscription').update(
@@ -274,15 +297,6 @@ router.post(['/', '/:action'], function(req, res, next) {
 
 											console.log("Insert sensor in existing hub ");
 
-//											db.people.findAndModify({
-//											query: { name: "Tom", state: "active", rating: { $gt: 10 } },
-//											sort: { rating: 1 },
-//											update: { $inc: { score: 1 } }
-//											})
-
-//											db.collection('subscription').find( {"userid": userid, "dataSetId": tempSensorDataSetID,"subscribedto": {"sensorId":tempSensorId}} ).update( { $set: { status: "I", points: "0" } } );
-
-
 //											console.log("tempSensorStation: "+tempSensorStation);
 //											console.log("tempSensorDataSetID: "+tempSensorDataSetID);
 //											console.log("tempSensorLat: "+tempSensorLat);
@@ -301,14 +315,10 @@ router.post(['/', '/:action'], function(req, res, next) {
 														subscribedto:{sensorId:tempSensorId,sensorname:tempSensorName}
 													}}
 											);
-
-
-
 										}
 									}
 
 								});
-
 							}
 							else
 							{
@@ -318,202 +328,16 @@ router.post(['/', '/:action'], function(req, res, next) {
 								console.log("-------------Insert everything -- NEW ROW---------------------");
 								db.collection('subscription').insert( {userid: userid, dataSetId: tempSensorDataSetID, location: {lat:tempSensorLat,lng:tempSensorLng},from:tempFrom,to:tempTo,subscribedto:[{sensorId:tempSensorId,sensorname:tempSensorName}]});
 							}
-
-							/*{
-							if(err==null)
-								{
-								console.log("Error while inserting a row for userid:"+userid+" tempSensorDataSetID: "+tempSensorDataSetID+""+);
-
-								}
-								});*/
-
 						}
 					});
 				}
 			}
-			else
-			{
-				console.log("----------------------------------");
-				console.log("----------------------------------");
-
-
-				tempSensorDataSetID=req.body.check.split("+")[0];
-				tempSensorLat=req.body.check.split("+")[1];
-				tempSensorLng=req.body.check.split("+")[2];
-				tempSensorId=req.body.check.split("+")[3];
-
-
-
-
-
-				userid="571dc7758e70a5e6101dcac1";
-				tempSensorStation="HARD CODED";
-				tempSensorName="HARD CODED";
-
-				//tempSensorDataSetID="gov_noaa_nws_kmry";
-				//tempSensorLat="36.5833333333333";
-				//tempSensorLng="-121.85";
-
-				//tempSensorName="Wind Speed (Knot)";
-				//tempSensorId="wind_speed";
-
-				//tempSensorName="Air Temperature (Degree F)";
-				//tempSensorId="air_temperature";
-
-				console.log("tempSensorStation: "+tempSensorStation);
-				console.log("tempSensorDataSetID: "+tempSensorDataSetID);
-				console.log("tempSensorLat: "+tempSensorLat);
-				console.log("tempSensorLng: "+tempSensorLng);
-				console.log("tempSensorId: "+tempSensorId);
-				console.log("tempSensorName: "+tempSensorName);
-
-				console.log("----------------------------------");
-				console.log("----------------------------------");
-
-
-				//var cursor =db.collection('subscription').find("station", { latitude: 38.0657, longitude :-122.2302}).toArray(function(err,result){
-				//var dataSetId =db.collection('subscription').find("dataSetId",{"userid": "571dc7758e70a5e6101dcac1", "dataSetId": tempSensorDataSetID}).toArray(function(err,result){
-				db.collection('subscription').distinct( "dataSetId", {"userid": userid, "dataSetId": tempSensorDataSetID} ,function(err, result) {
-
-
-					if(err!=null)
-					{
-						console.log("There is some error from Database. Error is = "+err);
-					}
-					else
-					{
-						console.log("----------------------.....................");
-						console.log("result:"+result);
-						if(result==tempSensorDataSetID)
-						{
-							//DatasetID row already present.
-
-							console.log(tempSensorDataSetID+" Already Present");
-
-							//Check if the sensor exists, else insert a new sensor
-
-							db.collection('subscription').distinct( "dataSetId", {"userid": userid, "dataSetId": tempSensorDataSetID,"subscribedto": {"sensorId":tempSensorId}} ,function(err1, result1) {
-
-								console.log("result1 is :"+result1);
-
-								if(err1!=null)
-								{
-									console.log("There is some error from Database. Error is = "+err1);
-								}
-								else
-								{
-									if(result1==tempSensorDataSetID)
-									{
-										//Sensor Name found
-										//Do nothing
-
-										console.log("Updating");
-
-										db.collection('subscription').update(
-												{"userid": userid, "dataSetId": tempSensorDataSetID},
-
-												{$set:{
-													from:tempFrom,to:tempTo
-												}}
-										);
-
-
-//										db.collection('subscription').findAndModify({
-//										query: {"userid": userid, "dataSetId": tempSensorDataSetID,"subscribedto": {"sensorId":tempSensorId}},
-//										update:{$set: {from:tempFrom,to:tempTo,subscribedto:{sensorId:tempSensorId,sensorname:tempSensorName}}}
-//										});
-
-										console.log("Sensor Name found. The sensor name is: "+result1);
-									}
-									else
-									{
-										//Insert a new sensor in existing Station
-
-										console.log("Insert sensor in existing hub ");
-
-//										db.people.findAndModify({
-//										query: { name: "Tom", state: "active", rating: { $gt: 10 } },
-//										sort: { rating: 1 },
-//										update: { $inc: { score: 1 } }
-//										})
-
-//										db.collection('subscription').find( {"userid": userid, "dataSetId": tempSensorDataSetID,"subscribedto": {"sensorId":tempSensorId}} ).update( { $set: { status: "I", points: "0" } } );
-
-										db.collection('subscription').update(
-												{"userid": userid, "dataSetId": tempSensorDataSetID},
-
-												{$push:{
-													subscribedto:{sensorId:tempSensorId,sensorname:tempSensorName}
-												}}
-										);
-
-
-
-									}
-								}
-
-							});
-
-						}
-						else
-						{
-							console.log(tempSensorDataSetID+" Not Present");
-							//Insert everything -- NEW ROW
-
-							console.log("-------------Insert everything -- NEW ROW---------------------");
-							db.collection('subscription').insert( {userid: userid, dataSetId: tempSensorDataSetID, location: {lat:tempSensorLat,lng:tempSensorLng},from:tempFrom,to:tempTo,subscribedto:{sensorId:tempSensorId,sensorname:tempSensorName}});
-						}
-
-						/*{
-						if(err==null)
-							{
-							console.log("Error while inserting a row for userid:"+userid+" tempSensorDataSetID: "+tempSensorDataSetID+""+);
-
-							}
-							});*/
-
-					}
-				});
-
-			}
-
-
 
 			res.status(200).redirect('/sensor/getSensors');
-			/*res.status(200).render("editor/sensorInstance.jade", {
-			res.('/nodepost.js?a=1
-
-			pageTitle: "Sensed! - Dashboard",
-			showRegister: true,
-			showlogin:false
-		});*/
-
-
-			// var cursor =db.collection('subscription').find({"userid":""+userdoc._id}).toArray(function(err,result){
-			//   //console.log("->" + JSON.stringify(result));
-			//   if(err==null){
-			//   res.status(200).render("data/display.jade", {
-			//   subscription: result,
-			//   pageTitle: "Sensed! - Dashboard",
-			//   showRegister: true,
-			//   showlogin:false
-			// });
-
-			// }
-			// else{
-			//   res.status(200).render("404.jade");
-			//   res.end();
-			// }
-			// db.close();
-			// //res.end();
-			// });
 		}
-
 		checkSession(req,res,findSubscription);
 
 		break;
-
-
 	}
 	});
 
